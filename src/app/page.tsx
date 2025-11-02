a534a7d3811f34fdaaee1f596bffff7fc3a5fa65
biru-codeastromer <birajitsaikia@gmail.com>
2025-11-02 18:13:18 +0530

    feat: initial Next.js project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-2xl font-bold">MLBoost Homepage</h1>
+      <p>The "LeetCode for ML" is under construction.</p>
+    </main>
+  );
+}
