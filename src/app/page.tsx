2686d458b868187290102990b04dd84ccd8cb5c9
Birajit Saikia <birajitsaikia@gmail.com>
2025-11-02 19:11:39 +0530

    feat: initial Next.js project setup (#1)
    * feat: initial Next.js project setup
    
    * fixing lint
    
    ---------
    
    Co-authored-by: GAMMA_Demon <devaanshkathuria@gmail.com>

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-2xl font-bold">MLBoost Homepage</h1>
+      <p>The &quot;LeetCode for ML&quot; is under construction.</p>
+    </main>
+  );
+}
